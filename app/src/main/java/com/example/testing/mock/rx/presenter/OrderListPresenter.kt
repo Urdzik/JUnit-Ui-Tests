f3512d2dc150430437a186850354613e192a3682
Slavik Urdzik <urdziksv@gmail.com>
2020-04-10 10:37:44 +0300

    Started adding test for Presenter from MVP

--- app/src/main/java/com/example/testing/mock/rx/presenter/OrderListPresenter.kt
+++ app/src/main/java/com/example/testing/mock/rx/presenter/OrderListPresenter.kt
@@ -1,37 +1,41 @@
 package com.example.testing.mock.rx.presenter
 
-import android.annotation.SuppressLint
+import androidx.annotation.NonNull
+import com.example.testing.mock.rx.Order
 import com.example.testing.mock.rx.OrderListContract
 import com.example.testing.mock.rx.OrderListContract.Presenter
 import com.example.testing.mock.rx.OrderRepository
 import io.reactivex.Scheduler
+import io.reactivex.observers.DisposableObserver
 
 
 class OrderListPresenter(
     private val orderRepository: OrderRepository,
     private val workScheduler: Scheduler,
     private val resultScheduler: Scheduler
-) : BasePresenter<OrderListContract.View>(), Presenter {
-
-
-    @SuppressLint("CheckResult")
+) :
+    BasePresenter<OrderListContract.View>(), Presenter {
     override fun refresh() {
         if (view == null) return
         view!!.showProgress()
         orderRepository.getOrders()
             .subscribeOn(workScheduler)
             .observeOn(resultScheduler)
-            .subscribe({
-                if (view == null)
-
-                view!!.hideProgress()
-                view!!.showOrders(it)
-            },
-            {
-                    if (view == null)
-
+            .subscribe(object : DisposableObserver<List<Order>>() {
+                override fun onNext(@NonNull orders: List<Order>) {
+                    if (view == null) return
                     view!!.hideProgress()
-                    view!!.showError(it.message)
+                    view!!.showOrders(orders)
+                }
+
+                override fun onError(@NonNull e: Throwable) {
+                    if (view == null) return
+                    view!!.hideProgress()
+                    view!!.showError(e.message)
+                }
+
+                override fun onComplete() {}
             })
     }
+
 }
